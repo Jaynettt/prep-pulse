0f6d952c936ebeccbd220bffd545278037125e3b
Mmanare Mahlatse nkoana <mahlatsemmanarenkoana@gmail.com>
2024-09-30 16:48:55 +0200

    created remaining models and controllers

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_30_143212) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_30_144324) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "answers", force: :cascade do |t|
+    t.string "content"
+    t.bigint "question_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -40,6 +48,14 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_30_143212) do
     t.index ["user_id"], name: "index_pulses_on_user_id"
   end
 
+  create_table "questions", force: :cascade do |t|
+    t.string "content"
+    t.bigint "pulse_category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["pulse_category_id"], name: "index_questions_on_pulse_category_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -54,7 +70,9 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_30_143212) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "answers", "questions"
   add_foreign_key "pulse_categories", "categories"
   add_foreign_key "pulse_categories", "pulses"
   add_foreign_key "pulses", "users"
+  add_foreign_key "questions", "pulse_categories"
 end
