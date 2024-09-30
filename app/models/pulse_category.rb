2ed6d7794ccced3fc9acf97a2592f2b0c907a8a1
Mmanare Mahlatse nkoana <mahlatsemmanarenkoana@gmail.com>
2024-09-30 17:21:18 +0200

    created validations for questions etc model

--- app/models/pulse_category.rb
+++ app/models/pulse_category.rb
@@ -1,4 +1,7 @@
 class PulseCategory < ApplicationRecord
-  belongs_to :category
   belongs_to :pulse
+  belongs_to :category
+  has_many :questions
+  validates :pulse_id, presence: true
+  validates :category_id, presence: true
 end
