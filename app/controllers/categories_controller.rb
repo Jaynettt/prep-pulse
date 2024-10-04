1aff047920ae26aa7eebdc55b38485958b5c15ea
janet uche <uchejanet513@gmail.com>
2024-10-04 12:39:18 +0200

    updated dashboard

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,18 @@
 class CategoriesController < ApplicationController
-
   def index
     @categories = Category.all
+    # @category = Category.find(params[:id])
+    # if @question
+    #   puts "Redirecting to question with id: #{@question.id}"
+    #   # redirect_to question_path(@question)
+    # else
+    #   puts "No question found for category: #{@category.name}"
+    #   redirect_to categories_path, alert: 'No questions available for this category.'
+    # end
   end
 
+  def show
+    @question = Question.first
+    redirect_to question_path(@question)
+  end
 end
