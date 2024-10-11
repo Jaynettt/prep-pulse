4e19545eaf884b6071703c95f33f4f4aa3b40a22
Megan Street <meganjstreet@gmail.com>
2024-10-11 14:21:06 +0200

    working on making sure loader works and adding background gradient for question and answer pages

--- app/jobs/set_chat_review_job.rb
+++ app/jobs/set_chat_review_job.rb
@@ -2,29 +2,30 @@ class SetChatReviewJob < ApplicationJob
   queue_as :default
 
   def perform(answer)
-    client = OpenAI::Client.new
-    chatgpt_response = client.chat(parameters: {
-    model: "gpt-4",  # Use the correct model
-    messages: [{ role: "user", content: "Give some constructive feedback for this answer #{answer.content}. The interview question was #{answer.question.content}. Make it 500 characters maximum"}]
-    })
-    new_chat_review = chatgpt_response["choices"][0]["message"]["content"]
+    # Debounce logic: ensure that subsequent requests wait
+    Rails.cache.fetch("debounce_broadcast_#{answer.id}", expires_in: 5.seconds) do
+      client = OpenAI::Client.new
+      chatgpt_response = client.chat(parameters: {
+        model: "gpt-4",
+        messages: [{ role: "user", content: "Give some constructive feedback for this answer #{answer.content}. The interview question was #{answer.question.content}. Make it 500 characters maximum"}]
+      })
+      new_chat_review = chatgpt_response["choices"][0]["message"]["content"]
 
+      evaluation_response = client.chat(parameters: {
+        model: "gpt-4",
+        messages: [
+          { role: "user", content: "Rate the following answer on a scale of 1 to 10: '#{answer.content}'. Give me only the number." }
+        ]
+      })
+      new_evaluation = evaluation_response["choices"][0]["message"]["content"].to_i
 
+      answer.update!(evaluation: new_evaluation, chat_review: new_chat_review)
 
-    response = client.chat(parameters: {
-      model: "gpt-4",
-      messages: [
-        { role: "user", content: "Rate the following answer on a scale of 0sche to 10: '#{answer.content}'. Give me only the number." }
-      ]
-    })
-
-    new_evaluation = response["choices"][0]["message"]["content"].to_i
-
-    answer.update!(evaluation: new_evaluation, chat_review: new_chat_review) # Persist the new evaluation
-    Turbo::StreamsChannel.broadcast_update_to(
-      "result_pulse_#{answer.question.pulse.id}",
-      target: "result_pulse_#{answer.question.pulse.id}",
-      partial: "pulses/results", locals: { pulse: answer.question.pulse})
-
+      Turbo::StreamsChannel.broadcast_update_to(
+        "result_pulse_#{answer.question.pulse.id}",
+        target: "result_pulse_#{answer.question.pulse.id}",
+        partial: "pulses/results", locals: { pulse: answer.question.pulse }
+      )
+    end
   end
 end
