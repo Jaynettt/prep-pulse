301a6885d66dfdbe6d3d2f96c6e632f8f763ed82
Megan Street <meganjstreet@gmail.com>
2024-10-11 12:08:19 +0200

    sliding in and out questions and answers and some front end

---
+++ app/javascript/controllers/answer_controller.js
@@ -0,0 +1,20 @@
+import { Controller } from "@hotwired/stimulus";
+
+// Connects to data-controller="answer"
+export default class extends Controller {
+  static targets = ["slidingDiv"];
+
+  slideDown(event) {
+    event.preventDefault();
+
+    // Add the slide-out class to trigger the animation
+    this.slidingDivTarget.classList.add('slide-out');
+
+    // Navigate to the link after the animation completes
+    const url = event.currentTarget.href; // Get the link's URL
+
+    setTimeout(() => {
+      window.location.href = url; // Redirect to the URL
+    }, 500); // The timeout matches the CSS animation duration
+  }
+}
